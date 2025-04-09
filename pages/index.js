eab815c3e4fde888294c247030d3002e3544f804
Serkan Sevinc <125906804+serkansevincus1@users.noreply.github.com>
2025-04-09 13:00:34 +0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>SRE Insights</h1>
-      <p>Welcome to my site about SRE, Monitoring, and DevOps.</p>
-    </div>
-  );
-}
