719b603d28d98ff169960cf1714a69e1d71101c5
Serkan Sevinc <125906804+serkansevincus1@users.noreply.github.com>
2025-04-09 13:23:56 +0300

    Create page.js

---
+++ app/blog/page.js
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Blog</h1>
+      <p>Burada SRE, Monitoring, DevOps üzerine yazılarımı bulabilirsiniz.</p>
+    </main>
+  );
+}
