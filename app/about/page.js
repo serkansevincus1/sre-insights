64cb7ddc736b8afc8e0f05b7e440b4108e09ca5e
Serkan Sevinc <125906804+serkansevincus1@users.noreply.github.com>
2025-04-09 13:26:35 +0300

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Hakkımda</h1>
+      <p>Ben Serkan Sevinç. SRE ve DevOps alanında yazılar yazıyor, içerik üretiyorum.</p>
+    </main>
+  );
+}
