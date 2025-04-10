e82a0bbdf84e841c93d6d40b38a4fe356c18f769
Serkan Sevinc <125906804+serkansevincus1@users.noreply.github.com>
2025-04-10 21:30:42 +0300

    Update route.js

--- app/api/save-posts/route.js
+++ app/api/save-posts/route.js
@@ -4,32 +4,17 @@ import path from 'path';
 export async function POST(request) {
   try {
     const data = await request.json();
-
-    // posts.json dosyasının yolu
     const filePath = path.join(process.cwd(), 'public', 'data', 'posts.json');
-
-    // Dosyaya yaz
     writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
-
-    console.log('✅ posts.json başarıyla güncellendi.');
     return new Response(JSON.stringify({ success: true }), {
       status: 200,
-      headers: {
-        'Content-Type': 'application/json'
-      }
+      headers: { 'Content-Type': 'application/json' },
     });
   } catch (err) {
     console.error('🛑 Dosyaya yazarken hata:', err);
-    return new Response(JSON.stringify({ success: false, error: err.message }), {
-      status: 500,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+    return new Response(
+      JSON.stringify({ success: false, error: err.message }),
+      { status: 500, headers: { 'Content-Type': 'application/json' } }
+    );
   }
-}
-
-// GET metodu tanımlı değilse, 405 döndürülür
-export function GET() {
-  return new Response('Method Not AllowedSSSSSS', { status: 405 });
 }
